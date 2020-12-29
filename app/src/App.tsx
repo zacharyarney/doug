cdb7b9ead34b25daf5e3ded7a8fd1b0f4bc24a8d
zacharyarney <zachary.arney@gmail.com>
2020-12-28 20:09:27 -0600

    WIP

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Route, Switch } from 'react-router-dom';
+import { v4 as uuidv4 } from 'uuid';
 import './App.css';
 import { Login } from './components/Login/Login';
 
 function App() {
+  const [redditAuthState, setRedditAuthState] = useState('');
+
+  const loginExtension =
+    'api/v1/authorize?client_id=' +
+    process.env.REACT_APP_CLIENT_ID +
+    '&response_type=code&state=' +
+    redditAuthState +
+    '&redirect_uri=' +
+    process.env.REACT_APP_REDIRECT_URI +
+    '&duration=permanent&scope=' +
+    process.env.REACT_APP_SCOPE_STRING;
+
+  useEffect(() => {
+    setRedditAuthState(uuidv4());
+  }, []);
+
   return (
     <Switch>
       <Route
