6e800b5445d00512d6acad15e856a93062a0d4a8
zacharyarney <zachary.arney@gmail.com>
2020-12-28 20:09:23 -0600

    adds AuthContext

---
+++ app/src/context/AuthContext.ts
@@ -0,0 +1,7 @@
+import React, { useState, createContext } from 'react';
+
+export const AuthContext = createContext({});
+
+export function AuthProvider(props) {
+  const [auth, setAuth] = useState({})
+}
